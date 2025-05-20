6b413173f2fc66219cd9d5dbe8b8f9f78f2b6031
SafaeKar <safae.karkach.etu23@ensem.ac.ma>
2025-05-20 17:10:09 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
         welcomeMessage();
         System.out.println("");
                 System.out.println("");
+                        System.out.println("");
+
 
         Electronics elec1= new Electronics("PC ",10000,2,"DELL",2025);
         Food food1=new Food("lait",4,4,"11/10/2024");
